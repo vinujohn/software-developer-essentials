536820d3469c6c553a7fb0ef57d0c2dc31ff15cd
David Parnell <dparnell@miteksystems.com>
2018-12-06 13:10:06 -0800

    Making Bid

---
+++ lab1a/Models/Bid.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Models
+{
+    public class Bid
+    {
+        public double amount { get; set; }
+        public string bidder { get; set; }
+        public User User { get; set; }
+
+    }
+}
